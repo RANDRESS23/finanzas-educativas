a4273a1ece9f97ea2658bb53bcac52d47bd7dc89
Raúl Quimbaya <raulquimbaya4@gmail.com>
2023-10-22 17:54:18 -0500

    Change the next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    dangerouslyAllowSVG: true,
+    domains: [
+      'images.unsplash.com',
+      'tailwindui.com',
+      'images.pexels.com',
+      'guia.itfip.edu.co',
+      'imagenes.20minutos.es',
+      'demo.themesberg.com'
+    ]
+  }
+}
 
 module.exports = nextConfig
