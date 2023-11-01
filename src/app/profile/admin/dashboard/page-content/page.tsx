b27e886c22c2870698e17a2a7d31a42e4ac52b5e
Ricardo Rojas <rojasricor@gmail.com>
2023-11-01 15:51:35 -0500

    NextJS caching problem fixe

--- src/app/profile/admin/dashboard/page-content/page.tsx
+++ src/app/profile/admin/dashboard/page-content/page.tsx
@@ -2,6 +2,9 @@ import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 import Meta from "./Meta";
 
+// const revalidate = 60;
+export const dynamic = "force-dynamic";
+
 export default async function PageContent() {
   const session = await getServerSession();
 
