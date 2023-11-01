f76f1ae07a0fed64ab25db95d7b5bffbe2723509
Ricardo Rojas <rojasricor@gmail.com>
2023-11-01 15:21:39 -0500

    Fixed small bug that prevent deploy

--- src/components/Skeletons/Round.tsx
+++ src/components/Skeletons/Round.tsx
@@ -3,7 +3,7 @@ import Container from "./Container";
 export default function Round({ p = 7 }: { p?: number }) {
   return (
     <Container>
-      <div className="rounded-full p-7 bg-gray-200" />
+      <div className={`rounded-full p-${p} bg-gray-200`} />
     </Container>
   );
 }
