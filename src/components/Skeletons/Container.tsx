6004bae36a33215413f89a00021e1176aa5b1b16
Ricardo Rojas <rojasricor@gmail.com>
2023-11-01 20:51:34 -0500

    Loading added for content section

--- src/components/Skeletons/Container.tsx
+++
@@ -1,7 +0,0 @@
-export default function Container({ children }: { children: React.ReactNode }) {
-  return (
-    <div role="status" className="max-w-sm animate-pulse">
-      {children}
-    </div>
-  );
-}
--- src/components/Skeletons/Round.tsx
+++
@@ -1,9 +0,0 @@
-import Container from "./Container";
-
-export default function Round() {
-  return (
-    <Container>
-      <div className="rounded-full p-7 bg-gray-200" />
-    </Container>
-  );
-}
