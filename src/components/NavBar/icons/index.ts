9cde79cc1d8bf91b80e81c85c74142f40730865f
Raúl Quimbaya <raulquimbaya4@gmail.com>
2023-11-05 19:21:47 -0500

    Fixing small details

--- src/components/NavBar/icons/index.ts
+++ src/components/NavBar/icons/index.ts
@@ -6,5 +6,8 @@ export { default as CloseIcon } from './CloseIcon';
 export { default as FinanceIcon } from './FinanceIcon';
 export { default as LogInIcon } from './LogInIcon';
 export { default as MenuIcon } from './MenuIcon';
+export { default as MisionIcon } from './MisionIcon';
 export { default as PigIcon } from './PigIcon';
+export { default as QuestionIcon } from './QuestionIcon';
 export { default as UserIcon } from './UserIcon';
+export { default as VisionIcon } from './VisionIcon';
