0e373e88b5f3c6a8209cb786ded6ad066c7fb42c
Raúl Quimbaya <raulquimbaya4@gmail.com>
2023-11-13 23:02:10 -0500

    Update ProfileAdminPage layout and image size

--- src/app/profile/admin/page.tsx
+++ src/app/profile/admin/page.tsx
@@ -15,8 +15,8 @@ export default async function ProfileAdminPage() {
 
   return (
     <div className="mb-20">
-      <div className="relative isolate px-6 pb-20 lg:py-0 lg:pl-36 lg:flex lg:justify-between lg:gap-10 lg:h-screen">
-        <div className="max-w-xl h-screen lg:h-full flex flex-col justify-center items-center">
+      <div className="relative isolate px-6 pb-20 lg:py-0 lg:pl-36 lg:flex lg:justify-between lg:items-center lg:gap-10 lg:h-screen">
+        <div className="h-screen lg:h-full flex flex-col justify-center items-center">
           <div className="flex mb-8 justify-center">
             <div className="relative rounded-full px-5 py-1 text-sm leading-6 text-gray-600 dark:text-gray-400 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:ring-zinc-200/10 dark:hover:ring-gray-200/20 text-center">
               Completar mi información personal.{" "}
@@ -51,13 +51,13 @@ export default async function ProfileAdminPage() {
             </div>
           </div>
         </div>
-        <div className="sm:w-3/4 relative rounded-lg overflow-hidden">
+        <div className="lg:w-1/2 relative rounded-lg overflow-hidden flex justify-center items-center">
           <Image
-            width={500}
+            width={600}
             height={400}
             src="https://images.pexels.com/photos/4195342/pexels-photo-4195342.jpeg?auto=compress&cs=tinysrgb&w=600"
             alt="photo"
-            className="object-cover lg:h-full w-[180%] h-auto aspect-square transform hover:scale-105 hover:brightness-105 transition-all duration-300 cursor-pointer"
+            className="object-cover aspect-square transform hover:scale-105 hover:brightness-105 transition-all duration-300 cursor-pointer rounded-lg"
           />
         </div>
       </div>
