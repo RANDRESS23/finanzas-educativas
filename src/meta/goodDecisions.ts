030945add165625f09bdb499797b180c4912eb5d
Raúl Quimbaya <raulquimbaya4@gmail.com>
2023-11-26 18:44:05 -0500

    Adding the content of the second dimension

---
+++ src/meta/goodDecisions.ts
@@ -0,0 +1,81 @@
+import {
+  FinanceIcon,
+  PigMoneyIcon,
+} from "@/app/financial-education/first-dimension/icons";
+
+const goodDecisions = [
+  {
+    name: "Definir claramente el problema",
+    description:
+      "Hay que tener en claro el problema para así empezar a plantear objetivos y posibles decisiones a tomar.",
+    Icon: FinanceIcon,
+  },
+  {
+    name: "Establece tus objetivos",
+    description:
+      "Define claramente el objetivo que deseas lograr con la decisión. Comprender el propósito facilita la identificación de las opciones relevantes.",
+    Icon: FinanceIcon,
+  },
+  {
+    name: "Recopila información relevante",
+    description:
+      "Recopila información relevante y suficiente antes de tomar una decisión. Cuanta más información tengas, más fundamentada será tu elección.",
+    Icon: FinanceIcon,
+  },
+  {
+    name: "Identifica las opciones disponibles y/o alternativas",
+    description:
+      "Genera una lista de posibles opciones o alternativas. Cuantas más opciones consideres, más amplio será tu panorama y mejores serán las posibilidades de tomar una decisión informada.",
+    Icon: FinanceIcon,
+  },
+  {
+    name: "Evalúa las ventajas y desventajas",
+    description:
+      "Evalúa las ventajas y desventajas de cada opción. Considera factores como riesgos, beneficios, costos y consecuencias a corto y largo plazo.",
+    Icon: FinanceIcon,
+  },
+  {
+    name: "Consulta con otras personas",
+    description:
+      "Al consultar con otras personas abrirá tu percepción de ciertas alternativas que tenías en cuenta.",
+    Icon: FinanceIcon,
+  },
+  {
+    name: "Evalúa tus valores y principios",
+    description:
+      "",
+    Icon: PigMoneyIcon,
+  },
+  {
+    name: "Ten en cuenta el factor tiempo ",
+    description:
+      "",
+    Icon: PigMoneyIcon,
+  },
+  {
+    name: "Visualiza las consecuencias a largo plazo",
+    description:
+      "",
+    Icon: PigMoneyIcon,
+  },
+  {
+    name: "Confía en tu intuición",
+    description:
+      "",
+    Icon: PigMoneyIcon,
+  },
+  {
+    name: "Toma la decisión de forma racional",
+    description:
+      "Intenta tomar decisiones de manera objetiva y racional. Evita que las emociones o prejuicios influyan demasiado en tu elección.",
+    Icon: PigMoneyIcon,
+  },
+  {
+    name: "Aprende de la experiencia",
+    description:
+      "",
+    Icon: PigMoneyIcon,
+  },
+];
+
+export default goodDecisions;
