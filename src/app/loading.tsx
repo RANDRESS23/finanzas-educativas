0ec9f77eec2fe0872fef1095d543c1afbbbcc208
Ricardo Rojas <rojasricor@gmail.com>
2023-12-18 19:29:12 -0500

    changed loader

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -3,10 +3,10 @@ export default function LoadingPage() {
     <div className="top-0 left-0 w-screen h-screen flex items-center justify-center overflow-hidden">
       <div className="spinner-box" role="status">
         <div className="leo-border-1">
-          <div className="leo-core-1"></div>
+          <div className="leo-core-1" />
         </div>
         <div className="leo-border-2">
-          <div className="leo-core-2"></div>
+          <div className="leo-core-2" />
         </div>
       </div>
     </div>
